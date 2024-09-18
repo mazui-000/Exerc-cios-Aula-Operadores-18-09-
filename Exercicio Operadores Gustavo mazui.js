3d0c521a83fcd5382c91a2c3c33fbbecab0889eb
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-09-18 20:38:50 -0300

    Add files via upload

---
+++ Exercicio Operadores Gustavo mazui.js
@@ -0,0 +1,81 @@
+
+////////////// Exercícios de interpretação de código 1 
+
+/*
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2 /*"==!" transformou bool2 verdadeiro
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) /*será impresso no comando false
+ 
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) /*será impresso no comando false
+
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado) /*será impresso no comando true
+
+
+
+console.log("d. ", typeof resultado) /* será impresso boolean
+
+*/ 
+
+////////////// Exercicio 2
+
+/* let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) /* O resultado seria o primeiro e segundo numero concatenar.
+Para resolver isso, precisariamos transformar o tipo de variavel em Number */ 
+
+
+
+
+
+////////////// Exercícios de escrita de código 1 
+
+/* let idade = Number (prompt ("Qual é sua idade ?"))
+let idadeAmg = Number (prompt ("Qual é a idade do seu amigo"))
+let resultadoIdade = idade - idadeAmg
+
+console.log ("Sua idade é maior do que a do seu melhor amigo?" , idade > idadeAmg )
+console.log ("Vocês tem" , resultadoIdade , "anos de diferença de idade")*/ 
+
+
+
+
+////////////// Exercícios 2 
+
+/* let par = Number (prompt ("Insira um numero Par"))
+let resultado = par % 2
+console.log (resultado) /* O padrão é que todos os pares colocados darão 0, enquanto os impares darão 1 */ 
+
+
+////////////// Exercícios 3 */ 
+
+/* let ano = Number(prompt ("Qual é sua idade"))
+let meses = ano * 12 
+let dias = meses * 365
+let horas = dias * 24
+
+console.log ("Idade em Meses" , meses , "Dias" , dias , "Horas" , horas) */ 
+
+
+
+////////////// Exercícios 4
+
+/* let primeiroNumero = Number(prompt("Insira um numero"))
+let segundoNumero = Number(prompt("Insira mais um numero"))
+
+console.log ("A. O primeiro numero é maior que o segundo ?" , primeiroNumero > segundoNumero)
+console.log ("B. O numero é igual ao segundo ?" , primeiroNumero === segundoNumero) 
+console.log ("C. O primeiro numero é divisível pelo segundo?" , primeiroNumero % segundoNumero ===0)
+console.log ("D. O segundo numero é divisível pelo primeiro ?" , segundoNumero % primeiroNumero ===0) */ 
+
+
+
